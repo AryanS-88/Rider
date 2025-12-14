caee93b11d27db83983ab08862686e408ad5a4d1
AryanS-88 <ssingharyan70@gmail.com>
2025-12-14 23:32:31 +0530

    assessts

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { StatusBar, Text, View } from 'react-native'
 
-export default function index() {
+const index = () => {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-4xl font-bold text-red-500">Namastae</Text>
+      <Text className="text-4xl font-bold text-red-500">Namas</Text>
       <StatusBar style="auto" />
     </View>
   )
 }
+export default index
